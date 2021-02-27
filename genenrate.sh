1f3efe977202d6fbac97469f72c555adce8542cd
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-02-27 18:24:28 +0900

    inital commit

---
+++ genenrate.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "<!DOCTYPE html>"
+echo "<html lang="ko">"
+echo "<head>"
+echo "<meta charset=\"utf-8\">"
+echo "<title>$1</title>"
+echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+echo "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\">"
+echo "</head>"
+echo "<body><pre>"
+echo "$( \
+	cat "$1" \
+		| sed -E 's/``([^`]*)``/<code>\1<\/code>/g' \
+		| sed -E 's/\*\*([^\*]*)\*\*/<b>\1<\/b>/g' \
+		| sed -E 's/--([^-]*)--/<s>\1<\/s>/g' \
+		| sed -E 's/__([^_]*)__/<u>\1<\/u>/g' \
+		| sed -E 's/\[\[(.+)\]\]/<a href="\1">\1<\/a>/g' \
+)"
+echo "</pre></body>"
+echo "</html>"
